9b5579f3f83637cc2903563873fa350dc15e5ce4
hedgehog34 <rjezyk@vp.pl>
2018-05-14 23:45:35 +0100

    start of application;

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -5,14 +5,34 @@ import { Ask } from "../Ask/Ask";
 import styles from './App.css';
 
 class App extends Component {
+    state = {
+        question: '',
+        askee: '',
+        submit: '',
+        questions: {},
+    };
+
+    handleInputChange = evt => {
+        this.setState({ [evt.target.id]: evt.target.value });
+    };
+
+    handleQuestionSubmit = evt => {
+        // console.log(evt.target.id);
+        // this.setState({ [evt.target.id]: !this.state[evt.target.id] })
+        evt.preventDefault();
+        this.setState({ submit: evt.target.id });
+    };
+
     render() {
+        console.log(this.state);
         return (
             <div className={styles.app}>
                 <header className={styles.header}>
                     <img src={logo} className={styles.logo} alt="logo"/>
                     <h1 className={styles.title}>Rejection Application</h1>
                 </header>
-                <Ask />
+                <Ask handleInputChange={this.handleInputChange}
+                     handleQuestionSubmit={this.handleQuestionSubmit}/>
             </div>
         );
     }
--- app/src/components/Ask/Ask.jsx
+++ app/src/components/Ask/Ask.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Ask.css';
 
 
-export const Ask = () => {
-    return (
-        <div className={styles.ask}>My Ask</div>
-    );
-};
+export class Ask extends Component {
+    render() {
+        const { handleInputChange, handleQuestionSubmit } = this.props;
+        return (
+            <div className={styles.ask}>
+                <div className={styles.labelInputPair}>
+                    <label htmlFor="question"
+                           className={styles.label}>Rejection Question</label>
+                    <input id="question"
+                           className={styles.input}
+                           type="text"
+                           onChange={handleInputChange}/>
+                </div>
+
+                <div className={styles.labelInputPair}>
+                    <label htmlFor="askee"
+                           className={styles.label}>Name of the person asked</label>
+                    <input id="askee"
+                           className={styles.input}
+                           type="text"
+                           onChange={handleInputChange}/>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button id="accepted"
+                            className={styles.button}
+                            onClick={handleQuestionSubmit}>
+                        Accepted
+                    </button>
+                    <button  id="rejected"
+                             className={styles.button}
+                            onClick={handleQuestionSubmit}>
+                        Rejected
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
